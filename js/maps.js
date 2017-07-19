c023a0fbc2b21fddd5fb7ec9940e226c6e4b52ad
KevTLW <kevthelonewolf@gmail.com>
2017-07-19 10:36:01 -0400

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -65,7 +65,7 @@ L.marker([40.73292,-73.983202], {icon: L.AwesomeMarkers.icon({icon: "hospital-o"
 			// Mount Sinai Beth Israel
 
 			// Mount Sinai Hospital
-L.marker([40.7694605,-74.0565464], {icon: L.AwesomeMarkers.icon({icon: "hospital-o", prefix: "fa", markerColor: "darkblue"}) }).addTo(mMap)
+L.marker([40.7899116,-73.9546519], {icon: L.AwesomeMarkers.icon({icon: "hospital-o", prefix: "fa", markerColor: "darkblue"}) }).addTo(mMap)
   .bindPopup("Mount Sinai Hospital<br><a target='_blank' href='https://www.google.com/maps/dir//The+Mount+Sinai+Hospital,+1468+Madison+Avenue,+New+York,+NY+10029/@40.7897101,-74.0227334,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89c2f63dcaeeda93:0x9797c11e6d7bc63f!2m2!1d-73.9526931!2d40.7897313'>Directions</a>")
 			// Mount Sinai Hospital
 
